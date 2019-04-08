e0098d12d4e2e969e6b9ecb5e5d05e04106e82ab
binhthgc <binhthgc@binhthgcs-MacBook-Pro.local>
2019-04-09 06:22:09 +0700

    table api

---
+++ dspace-rest/src/main/java/org/dspace/utils/DataUtils.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.dspace.utils;
+
+import java.io.Serializable;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.log4j.Logger;
+import org.dspace.core.Context;
+import org.dspace.rest.MyTableResource;
+import org.dspace.storage.rdbms.DatabaseManager;
+import org.dspace.storage.rdbms.TableRow;
+import org.dspace.storage.rdbms.TableRowIterator;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+/**
+ *
+ * @author binhthgc
+ */
+public class DataUtils {
+    
+    private static Logger log = Logger.getLogger(DataUtils.class);
+    
+    public static JSONArray findAll(Context context, Integer limit, Integer offset, String table, String cols) throws SQLException
+    {
+        JSONArray results = new JSONArray();
+        TableRowIterator tri = null;
+        List<org.dspace.content.Collection> collections = null;
+        List<Serializable> params = new ArrayList<Serializable>();
+        StringBuffer query = new StringBuffer(
+            "SELECT " + cols +
+            "FROM " + table +
+            limit != null ? " LIMIT " + offset + ", " + limit + " " : ""
+        );
+
+        DatabaseManager.applyOffsetAndLimit(query, params, offset, limit);
+
+        try
+        {
+            tri = DatabaseManager.query(
+              context, query.toString(), params.toArray()
+            );
+
+            collections = new ArrayList<org.dspace.content.Collection>();
+
+            while (tri.hasNext())
+            {
+                TableRow row = tri.next();
+                
+                String[] col = cols.split(",");
+                
+                JSONObject current = new JSONObject();
+                
+                for (String key : col) {
+                    
+                    current.put(key, row.getStringColumn(key));
+                            
+                }
+                
+                results.put(current);
+            }
+                
+            System.out.println("results:" + results);
+        }
+        catch (SQLException e)
+        {
+            log.error("Find all Collections offset/limit - ", e);
+            throw e;
+        }
+        finally
+        {
+            // close the TableRowIterator to free up resources
+            if (tri != null)
+            {
+                tri.close();
+            }
+        }
+
+        return results;
+    }
+}
