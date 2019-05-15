4fb104ceb655146f046df6a2309a34bad04cf01b
binhthvuejx <binhth.vuejx@gmail.com>
2019-05-15 10:49:06 +0700

    c

--- dspace-rest/src/main/java/org/dspace/utils/ElasticQueryWrapUtil.java
+++ dspace-rest/src/main/java/org/dspace/utils/ElasticQueryWrapUtil.java
@@ -18,13 +18,20 @@ public class ElasticQueryWrapUtil {
 
 	private static final String ELASTIC_SERVER_API_URL = "http://localhost:8080/solr/search/query";
 	
-	public static JSONObject query(String q) {
+	public static JSONObject query(String q, String sort, String rows) {
 
 		JSONObject result = null;
 		try {
 			
 			byte[] postData       = q.getBytes( StandardCharsets.UTF_8 );
 			int    postDataLength = postData.length;
+			
+			if (!sort.equalsIgnoreCase("")) {
+				q = q + "&sort=" + sort;
+			}
+			if (!rows.equalsIgnoreCase("")) {
+				q = q + "&rows=" + rows;
+			}
 			
 			URL url = new URL(ELASTIC_SERVER_API_URL + "?q=" + q);
 
