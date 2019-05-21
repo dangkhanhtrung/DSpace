b8057ead8195d7d57c48eb538a06b8d673b0519f
binhthvuejx <binhth.vuejx@gmail.com>
2019-05-21 09:22:33 +0700

    ccc

--- dspace-api/src/main/java/org/dspace/content/DCDate.java
+++ dspace-api/src/main/java/org/dspace/content/DCDate.java
@@ -520,7 +520,7 @@ public class DCDate
           // forcibly truncate month name to 3 chars -- XXX FIXME?
         String monthName = getMonthName(getMonthUTC(), locale);
         System.out.println("DCDate.displayUTCDate()" + locale.getDisplayLanguage());
-        if (monthName.length() > 2)
+        if (monthName.length() > 2 && !locale.getDisplayLanguage().equalsIgnoreCase("vi_VN"))
             monthName = monthName.substring(0, 3);
 
         // display date and time
