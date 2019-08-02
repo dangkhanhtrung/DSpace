f0167ceaf8e423db5c76cac494674f43b42290d4
binhthvuejx <binhth.vuejx@gmail.com>
2019-08-02 12:50:38 +0700

    123213123

--- dspace-rest/src/main/java/org/dspace/rest/MyTableResource.java
+++ dspace-rest/src/main/java/org/dspace/rest/MyTableResource.java
@@ -195,6 +195,7 @@ public class MyTableResource extends Resource
                         add_value_by_xmlname(context,objectBody, cris_do_id, crisType, "text", "patent_Status", "patentsstatus");
                         add_value_by_xmlname(context,objectBody, cris_do_id, crisType, "text", "patent_Issuer", "patentsissuer");
                         add_value_by_xmlname(context,objectBody, cris_do_id, crisType, "text", "patent_CountryCode", "patentscountrycode");
+                        crisSearchService.updateCrisIndexPublic(context, objectBody.getString("patent_ID"));
                     }
                     
                 } else {
@@ -228,9 +229,9 @@ public class MyTableResource extends Resource
                     add_value_by_xmlname(context,objectBody, cris_do_id, crisType, "text", "patent_Status", "patentsstatus");
                     add_value_by_xmlname(context,objectBody, cris_do_id, crisType, "text", "patent_Issuer", "patentsissuer");
                     add_value_by_xmlname(context,objectBody, cris_do_id, crisType, "text", "patent_CountryCode", "patentscountrycode");
+                    crisSearchService.updateCrisIndexPublic(context, objectBody.getString("patent_ID"));
                 }
                 //crisSearchService.updateCrisIndexPublic(context, true);
-                crisSearchService.updateCrisIndexPublic(context, objectBody.getString("patent_ID"));
                 
             } else if (entity_object.equalsIgnoreCase("project")) {
             	
