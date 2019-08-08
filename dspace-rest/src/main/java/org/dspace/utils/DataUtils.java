9872c99a830965e4dbbaa83bc0fb9a68ce7fe1c0
binhthvuejx <binhth.vuejx@gmail.com>
2019-08-08 13:38:11 +0700

    123131

--- dspace-rest/src/main/java/org/dspace/utils/DataUtils.java
+++ dspace-rest/src/main/java/org/dspace/utils/DataUtils.java
@@ -45,7 +45,7 @@ public class DataUtils {
             "WHERE 1=1 " +
             "AND entity='" + entity + "' " +
             "AND view_detail=" + view +
-            " LIMIT 2 "
+            " LIMIT 12 "
         ;
         
         System.out.println("SQL: " + query.toString());
